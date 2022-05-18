42cc265ae27225329281bfcb6d71c2da6a222a29
lchahua <62721646+edededd@users.noreply.github.com>
2022-05-17 21:55:25 -0400

    cambio

--- src/main/java/Strategy.java
+++ src/main/java/Strategy.java
@@ -1,2 +1,45 @@
-public class Strategy {
+
+public interface Strategy{
+
+    public void execute();
+
 }
+
+class PromApertura implements Strategy{
+
+
+
+    @Override
+    public void execute(){
+        //eae
+    }
+
+
+}
+
+class PromCierre implements Strategy{
+
+
+
+    @Override
+    public void execute(){
+        //eae
+    }
+
+
+}
+
+class MedMovil implements Strategy{
+
+
+
+    @Override
+    public void execute(){
+        //eae
+    }
+
+
+}
+
+
+
